c94eac3918bc8cd90126950a9405f90b66d8eccb
Ronan Le Gall <rlegall@nuxeo.com>
2011-07-20 14:18:58 +0200

    Display no document message in news gadget and article gadget

--- nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/articles/articles.js
+++ nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/articles/articles.js
@@ -12,62 +12,75 @@ var NXRequestParams={ operationId : 'Social.Provider',            // id of opera
   usePagination : true,                                                 // manage pagination or not
   displayMethod : displayArticles,                                  // js method used to display the result
   displayColumns : [ { type: 'builtin', field: 'icon'},                 // minimalist layout listing
-                   { type: 'builtin', field: 'titleWithLink', label: '__MSG_label.dublincore.title__'},
-                   { type: 'date', field: 'dc:modified', label: '__MSG_label.dublincore.modified__'},
-                   { type: 'text', field: 'dc:creator', label: '__MSG_label.dublincore.creator__'},
-                   { type: 'text', field: 'note:note', label: '__MSG_label.article.abstract__'}
-                   ]
+                   { type: 'builtin', field: 'titleWithLink', label: prefs.getMsg('label.dublincore.title')},
+                   { type: 'date', field: 'dc:modified', label: prefs.getMsg('label.dublincore.modified')},
+                   { type: 'text', field: 'dc:creator', label: prefs.getMsg('label.dublincore.creator')},
+                   { type: 'text', field: 'note:note', label: prefs.getMsg('label.article.abstract')}
+                   ],
+                   noEntryLabel: prefs.getMsg('label.gadget.no.document')
 };
 
 function displayArticles(entries, nxParams) {
 
   var html = "";
-  for (var i = 0; i < entries.length; i++) {
-    var entry = entries[i];
-    var date = new Date(entry.properties["dc:modified"]);
-    html += '<div class="article">';
-    html += '<p>' + entry.properties["dc:creator"] + ', ' + date.toLocaleDateString() + ' - ';
-    html += '<a target ="_top" class="article_title" title="';
-    html += entry.title;
-    html += "\" href=\"";
-    html += NXGadgetContext.clientSideBaseUrl;
-    html += "nxpath/default";
-    html += entry.path;
-    html += "@view_documents";
-    html += "\" >";
-    html += entry.title;
-    html += '</a></p>';
-    var text = entry.properties["note:note"];
-    text = text.substring(0, text.length > 300 ? 300 : text.length);
-    html += '<p>' + text + '</p>';
-    html += '</div>';
-  }
+  if(entries && entries.length <= 0){
 
-  _gel('navFirstPage').onclick = function(e) {
-      firstPage(nxParams)
-  };
-  _gel('navPrevPage').onclick = function(e) {
-      prevPage(nxParams)
-  };
-  _gel('navNextPage').onclick = function(e) {
-      nextPage(nxParams)
-  };
-  _gel('navLastPage').onclick = function(e) {
-      lastPage(nxParams)
-  };
-
-  if (nxParams.usePagination) {
-      _gel('nxDocumentListPage').innerHTML = (currentPage + 1) + "/" + maxPage;
-  } else {
-      console.log("hide nav controls");
+    nxParams.noEntryLabel = nxParams.noEntryLabel || 'Nothing to show.';
+      html = '<p>' + nxParams.noEntryLabel + '</p> </br>';
       _gel('pageNavigationControls').style.display = 'none';
-  }
-
+    }else{
+      html=displayListOfArticles(entries,nxParams);
+    }
   _gel("nxDocumentListData").innerHTML = html;
 
   _gel("nxDocumentList").style.display = 'block';
 
   gadgets.window.adjustHeight();
+}
+
+function displayListOfArticles(entries,nxParams){
+  var html ="";
+  for (var i = 0; i < entries.length; i++) {
+      var entry = entries[i];
+      var date = new Date(entry.properties["dc:modified"]);
+      html += '<div class="article">';
+      html += '<p>' + entry.properties["dc:creator"] + ', ' + date.toLocaleDateString() + ' - ';
+      html += '<a target ="_top" class="article_title" title="';
+      html += entry.title;
+      html += "\" href=\"";
+      html += NXGadgetContext.clientSideBaseUrl;
+      html += "nxpath/default";
+      html += entry.path;
+      html += "@view_documents";
+      html += "\" >";
+      html += entry.title;
+      html += '</a></p>';
+      var text = entry.properties["note:note"];
+      text = text.substring(0, text.length > 300 ? 300 : text.length);
+      html += '<p>' + text + '</p>';
+      html += '</div>';
+    }
+
+    _gel('navFirstPage').onclick = function(e) {
+        firstPage(nxParams)
+    };
+    _gel('navPrevPage').onclick = function(e) {
+        prevPage(nxParams)
+    };
+    _gel('navNextPage').onclick = function(e) {
+        nextPage(nxParams)
+    };
+    _gel('navLastPage').onclick = function(e) {
+        lastPage(nxParams)
+    };
+    if (nxParams.usePagination) {
+        _gel('nxDocumentListPage').innerHTML = (currentPage + 1) + "/" + maxPage;
+    } else {
+        console.log("hide nav controls");
+        _gel('pageNavigationControls').style.display = 'none';
+    }
+
+  return html;
 }
 
 // execute automation request onload
--- nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/news/news.js
+++ nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/news/news.js
@@ -18,58 +18,71 @@ var NXRequestParams={ operationId : 'Social.Provider',            // id of opera
                    { type: 'date', field: 'dc:modified', label: '__MSG_label.dublincore.modified__'},
                    { type: 'text', field: 'dc:creator', label: '__MSG_label.dublincore.creator__'},
                    { type: 'text', field: 'note:note', label: '__MSG_label.article.abstract__'}
-                   ]
+                   ],
+                   noEntryLabel: prefs.getMsg('label.gadget.no.document')
 };
 
 function displayArticles(entries, nxParams) {
 
   var html = "";
-  for (var i = 0; i < entries.length; i++) {
-    var entry = entries[i];
-    var date = new Date(entry.properties["dc:modified"]);
-    html += '<div class="news">';
-    html += '<p>' + entry.properties["dc:creator"] + ', ' + date.toLocaleDateString() + ' - ';
-    html += '<a target ="_top" class="news_title" title="';
-    html += entry.title;
-    html += "\" href=\"";
-    html += NXGadgetContext.clientSideBaseUrl;
-    html += "nxpath/default";
-    html += entry.path;
-    html += "@view_documents";
-    html += "\" >";
-    html += entry.title;
-    html += '</a></p>';
-    var text = entry.properties["note:note"];
-    text = text.substring(0, text.length > 300 ? 300 : text.length);
-    html += '<p>' + text + '</p>';
-    html += '</div>';
-  }
+  if(entries && entries.length <= 0){
 
-  _gel('navFirstPage').onclick = function(e) {
-      firstPage(nxParams)
-  };
-  _gel('navPrevPage').onclick = function(e) {
-      prevPage(nxParams)
-  };
-  _gel('navNextPage').onclick = function(e) {
-      nextPage(nxParams)
-  };
-  _gel('navLastPage').onclick = function(e) {
-      lastPage(nxParams)
-  };
-
-  if (nxParams.usePagination) {
-      _gel('nxDocumentListPage').innerHTML = (currentPage + 1) + "/" + maxPage;
-  } else {
-      console.log("hide nav controls");
+    nxParams.noEntryLabel = nxParams.noEntryLabel || 'Nothing to show.';
+      html = '<p>' + nxParams.noEntryLabel + '</p> </br>';
       _gel('pageNavigationControls').style.display = 'none';
-  }
-
+    }else{
+      html=displayListOfArticles(entries,nxParams);
+    }
   _gel("nxDocumentListData").innerHTML = html;
 
   _gel("nxDocumentList").style.display = 'block';
 
   gadgets.window.adjustHeight();
+}
+
+function displayListOfArticles(entries,nxParams){
+  var html ="";
+  for (var i = 0; i < entries.length; i++) {
+      var entry = entries[i];
+      var date = new Date(entry.properties["dc:modified"]);
+      html += '<div class="article">';
+      html += '<p>' + entry.properties["dc:creator"] + ', ' + date.toLocaleDateString() + ' - ';
+      html += '<a target ="_top" class="article_title" title="';
+      html += entry.title;
+      html += "\" href=\"";
+      html += NXGadgetContext.clientSideBaseUrl;
+      html += "nxpath/default";
+      html += entry.path;
+      html += "@view_documents";
+      html += "\" >";
+      html += entry.title;
+      html += '</a></p>';
+      var text = entry.properties["note:note"];
+      text = text.substring(0, text.length > 300 ? 300 : text.length);
+      html += '<p>' + text + '</p>';
+      html += '</div>';
+    }
+
+    _gel('navFirstPage').onclick = function(e) {
+        firstPage(nxParams)
+    };
+    _gel('navPrevPage').onclick = function(e) {
+        prevPage(nxParams)
+    };
+    _gel('navNextPage').onclick = function(e) {
+        nextPage(nxParams)
+    };
+    _gel('navLastPage').onclick = function(e) {
+        lastPage(nxParams)
+    };
+    if (nxParams.usePagination) {
+        _gel('nxDocumentListPage').innerHTML = (currentPage + 1) + "/" + maxPage;
+    } else {
+        console.log("hide nav controls");
+        _gel('pageNavigationControls').style.display = 'none';
+    }
+
+  return html;
 }
 
 // execute automation request onload
