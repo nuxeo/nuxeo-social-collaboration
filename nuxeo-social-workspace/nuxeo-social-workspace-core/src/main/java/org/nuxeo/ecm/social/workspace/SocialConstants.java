c10d4345901a61ece0e625ab3004b3ec8a70c7a2
Thomas Roger <troger@nuxeo.com>
2011-07-26 02:05:51 +0200

    NXP-7298: cleanup SocialConstants

--- nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/SocialConstants.java
+++ nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/SocialConstants.java
@@ -22,31 +22,34 @@ package org.nuxeo.ecm.social.workspace;
  */
 public class SocialConstants {
 
-    public static final String REQUEST_DOC_TYPE = "Request";
+    // Social Workspace
+    public static final String SOCIAL_WORKSPACE_FACET = "SocialWorkspace";
 
-    public static final String REQUEST_ROOT_NAME = "requests";
+    public static final String SOCIAL_WORKSPACE_TYPE = "SocialWorkspace";
 
-    public static final String REQUEST_SCHEMA = "request";
+    public static final String FIELD_SOCIAL_WORKSPACE_APPROVE_SUBSCRIPTION = "socialw:approveSubscription";
 
-    public static final String REQUEST_TYPE_JOIN = "joinRequest";
+    public static final String FIELD_SOCIAL_WORKSPACE_IS_PUBLIC = "socialw:isPublic";
 
-    public static final String REQUEST_TYPE_INVITATION = "invitation";
+    public static final String PRIVATE_SECTION_RELATIVE_PATH = "private-section/";
 
-    public static final String FIELD_REQUEST_USERNAME = "req:username";
+    public static final String PUBLIC_SECTION_RELATIVE_PATH = "public-section/";
 
-    public static final String FIELD_REQUEST_TYPE = "req:type";
-
-    public static final String FIELD_REQUEST_INFO = "req:info";
-
-    public static final String FIELD_REQUEST_PROCESSED_DATE = "req:processedDate";
-
-    public static final String FIELD_REQUEST_PROCESSED_COMMENT = "req:processedComment";
+    public static final String SOCIAL_WORKSPACE_ACL_NAME = "socialWorkspaceAcl";
 
     public static final String VALIDATE_SOCIAL_WORKSPACE_TASK_NAME = "validateSocialWorkspace";
 
+
+    // Social Document
+    public static final String SOCIAL_DOCUMENT_FACET = "SocialDocument";
+
+    public static final String FIELD_SOCIAL_DOCUMENT_IS_PUBLIC = "socialdoc:isPublic";
+
+
     public static final String ARTICLE_TYPE = "Article";
 
-    public static final String ARTICLE_SCHEMA = "article";
+    public static final String NEWS_ITEM_TYPE = "NewsItem";
+
 
     public static final String CONTENT_PICTURE_PICTURE_FIELD = "contentpict:picture";
 
@@ -58,27 +61,25 @@ public class SocialConstants {
 
     public static final String FIELD_DC_AUTHOR = "dc:author";
 
-    public static final String FIELD_SOCIAL_WORKSPACE_APPROVE_SUBSCRIPTION = "socialw:approveSubscription";
 
-    public static final String SOCIAL_WORKSPACE_ACL_NAME = "socialWorkspaceAcl";
+    // Subscription Request
+    public static final String REQUEST_DOC_TYPE = "Request";
 
-    public static final String SOCIAL_WORKSPACE_FACET = "SocialWorkspace";
+    public static final String REQUEST_ROOT_NAME = "requests";
 
-    public static final String SOCIAL_WORKSPACE_TYPE = "SocialWorkspace";
+    public static final String REQUEST_SCHEMA = "request";
 
-    public static final String FIELD_SOCIAL_WORKSPACE_IS_PUBLIC = "socialw:isPublic";
+    public static final String REQUEST_TYPE_JOIN = "joinRequest";
 
-    public static final String SOCIAL_DOCUMENT_FACET = "SocialDocument";
+    public static final String FIELD_REQUEST_USERNAME = "req:username";
 
-    public static final String FIELD_SOCIAL_DOCUMENT_IS_PUBLIC = "socialdoc:isPublic";
+    public static final String FIELD_REQUEST_TYPE = "req:type";
 
-    public static final String NEWS_ITEM_TYPE = "NewsItem";
+    public static final String FIELD_REQUEST_INFO = "req:info";
 
-    public static final String SOCIAL_SECTION_TYPE = "SocialSection";
+    public static final String FIELD_REQUEST_PROCESSED_DATE = "req:processedDate";
 
-    public static final String PRIVATE_SECTION_RELATIVE_PATH = "private-section/";
-
-    public static final String PUBLIC_SECTION_RELATIVE_PATH = "public-section/";
+    public static final String FIELD_REQUEST_PROCESSED_COMMENT = "req:processedComment";
 
     public static final String NEWS_ROOT_RELATIVE_PATH = "news-root/";
 
