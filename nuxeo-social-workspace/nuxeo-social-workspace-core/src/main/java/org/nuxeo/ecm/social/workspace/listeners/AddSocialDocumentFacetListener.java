b24812e4f7dce86066afd4e245967a76218340a9
Thomas Roger <troger@nuxeo.com>
2011-08-17 15:59:42 +0200

    NXP-7342: add SocialDocument facet only on allowed sub types of a SocialWorkspace

--- nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/AddSocialDocumentFacetListener.java
+++ nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/AddSocialDocumentFacetListener.java
@@ -20,8 +20,11 @@ import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.ABOUT_TO_CREATE;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED_BY_COPY;
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_MOVED;
 import static org.nuxeo.ecm.social.workspace.SocialConstants.SOCIAL_DOCUMENT_FACET;
+import static org.nuxeo.ecm.social.workspace.SocialConstants.SOCIAL_WORKSPACE_TYPE;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.nuxeo.ecm.core.api.ClientException;
@@ -33,13 +36,18 @@ import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.ecm.core.schema.FacetNames;
+import org.nuxeo.ecm.platform.types.Type;
+import org.nuxeo.ecm.platform.types.TypeManager;
 import org.nuxeo.ecm.social.workspace.adapters.SocialWorkspace;
 import org.nuxeo.ecm.social.workspace.service.SocialWorkspaceService;
 import org.nuxeo.runtime.api.Framework;
 
 /**
- * Listener add social document facet to documents, except those with folderish
- * facet, when there are created, copied or moved in a social workspace.
+ * Adds {@code SocialDocument} facet to documents created, copied or moved in a
+ * {@code SocialWorkspace}.
+ * <p>
+ * Do not add the facet if the document has the Folderish facet, or if it's not
+ * a
  *
  * @author <a href="mailto:rlegall@nuxeo.com">Ronan Le Gall</a>
  */
@@ -61,8 +69,10 @@ public class AddSocialDocumentFacetListener implements EventListener {
             return;
         }
 
+        List<String> allowedTypes = getSocialWorkspaceAllowedSubTypes();
         DocumentModel document = ((DocumentEventContext) ctx).getSourceDocument();
-        if (document.hasFacet(FacetNames.FOLDERISH)) {
+        if (document.hasFacet(FacetNames.FOLDERISH)
+                || !allowedTypes.contains(document.getType())) {
             return;
         }
         CoreSession session = document.getCoreSession();
@@ -72,11 +82,25 @@ public class AddSocialDocumentFacetListener implements EventListener {
 
         if (sws == null) {
             return;
-
         }
+
         document.addFacet(SOCIAL_DOCUMENT_FACET);
         if (DOCUMENT_MOVED.equals(eventName)) {
             session.saveDocument(document);
+        }
+    }
+
+    private List<String> getSocialWorkspaceAllowedSubTypes() {
+        try {
+            TypeManager typeManager = Framework.getService(TypeManager.class);
+            Collection<Type> allowedSubTypes = typeManager.findAllAllowedSubTypesFrom(SOCIAL_WORKSPACE_TYPE);
+            List<String> types = new ArrayList<String>();
+            for (Type type : allowedSubTypes) {
+                types.add(type.getId());
+            }
+            return types;
+        } catch (Exception e) {
+            throw new ClientRuntimeException(e);
         }
     }
 
