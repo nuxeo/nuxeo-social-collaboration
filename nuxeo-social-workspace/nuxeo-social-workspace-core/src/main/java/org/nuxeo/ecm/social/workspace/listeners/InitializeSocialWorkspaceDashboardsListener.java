19d702d9bea7b132230edd37f71c7087390b884e
Thomas Roger <troger@nuxeo.com>
2011-08-25 15:43:34 +0200

    NXP-7406: public and private dashboards default templates

--- nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/InitializeSocialWorkspaceDashboardsListener.java
+++ nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/InitializeSocialWorkspaceDashboardsListener.java
@@ -18,7 +18,12 @@
 package org.nuxeo.ecm.social.workspace.listeners;
 
 import static org.nuxeo.ecm.core.api.event.DocumentEventTypes.DOCUMENT_CREATED;
+import static org.nuxeo.ecm.social.workspace.SocialConstants.PRIVATE_DASHBOARD_SPACE_NAME;
+import static org.nuxeo.ecm.social.workspace.SocialConstants.PUBLIC_DASHBOARD_SPACE_NAME;
 
+import java.util.Locale;
+
+import org.jboss.seam.international.LocaleSelector;
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
@@ -26,8 +31,10 @@ import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.social.workspace.SocialConstants;
 import org.nuxeo.ecm.spaces.api.Constants;
 import org.nuxeo.ecm.spaces.api.Space;
+import org.nuxeo.ecm.spaces.helper.WebContentHelper;
 import org.nuxeo.opensocial.container.shared.layout.api.LayoutHelper;
 
 /**
@@ -55,9 +62,57 @@ public class InitializeSocialWorkspaceDashboardsListener implements
 
     private static void initializeDashboards(CoreSession session,
             DocumentModel doc) throws ClientException {
+
+        // add initial gadgets
+        Locale locale;
+        try {
+            locale = LocaleSelector.instance().getLocale();
+        } catch (Exception e) {
+            locale = Locale.ENGLISH;
+        }
+
+        if (PRIVATE_DASHBOARD_SPACE_NAME.equals(doc.getName())) {
+            initializePrivateDashboard(doc, session, locale);
+        } else if (PUBLIC_DASHBOARD_SPACE_NAME.equals(doc.getName())) {
+            initializePublicDashboard(doc, session, locale);
+        }
+    }
+
+    private static void initializePrivateDashboard(DocumentModel doc, CoreSession session,
+            Locale locale) throws ClientException {
+        Space space = doc.getAdapter(Space.class);
+        space.initLayout(LayoutHelper.buildLayout(LayoutHelper.Preset.X_3_DEFAULT));
+        // first column
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "news", 0, 0, 0);
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "articles", 0, 0, 1);
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "members", 0, 0, 2);
+        // second column
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "library", 0, 1, 0);
+        // third column
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "socialworkspaceminimessages", 0, 2, 0);
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "socialworkspaceactivitystream", 0, 2, 1);
+    }
+
+    private static void initializePublicDashboard(DocumentModel doc, CoreSession session,
+            Locale locale) throws ClientException {
         Space space = doc.getAdapter(Space.class);
         space.initLayout(LayoutHelper.buildLayout(LayoutHelper.Preset.X_2_66_33));
-        // add initial gadgets
+        // first column
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "news", 0, 0, 0);
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "publicarticles", 0, 0, 1);
+        // second column
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "join", 0, 1, 0);
+        WebContentHelper.createOpenSocialGadget(space, session, locale,
+                "publicdocuments", 0, 1, 1);
     }
 
 }
