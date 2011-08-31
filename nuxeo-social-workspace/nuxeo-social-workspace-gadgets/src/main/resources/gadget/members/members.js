36d3c1d8de8d68630d20ffe97ee96bb10a1b8bce
Ronan Le Gall <rlegall@nuxeo.com>
2011-08-31 10:16:46 +0200

    NXP-7444: create Selenium Tests and fix former ones

--- nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/members/members.js
+++ nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/members/members.js
@@ -5,58 +5,59 @@ var page = 0;
 delete users;
 
 var NXRequestParams = {
-	operationId : 'SocialWorkspace.Members',
+  operationId : 'SocialWorkspace.Members',
     operationParams : {
-    	contextPath : getTargetContextPath(),
-    	pageSize : 5
-	},
-	entityType : 'blob',
-	operationContext : {},
-	operationCallback : operationExecutedCallback
-}
+      contextPath : getTargetContextPath(),
+      pageSize : 5
+  },
+  entityType : 'blob',
+  operationContext : {},
+  operationCallback : operationExecutedCallback
+  }
 
 function operationExecutedCallback(response, nxParams){
-	users = response.data['users'];
-	page = response.data['page'];
-	pageMax = response.data['pageMax'];
-	displayUsers();
+  users = response.data['users'];
+  page = response.data['page'];
+  pageMax = response.data['pageMax'];
+  displayUsers();
 }
 
 function doSearch() {
-	 var query = _gel('query').value;
-	 if ( isEmpty(query) ) {
-		delete users;
-		displayUsers();
-		hideErrorMessage();
-	 } else {
-	  	page = 0;
+  var query = _gel('query').value;
+  if ( isEmpty(query) ) {
+    delete users;
+    displayUsers();
+    hideErrorMessage();
+  } else {
+      page = 0;
         NXRequestParams.operationParams.pattern = query;
         getUsers();
-	 }
+  }
 }
 
 
 function displayUsers(){
-	if ( typeof users == 'undefined' ) {
-	 	_gel("message").style.display = 'none';
-		_gel("list").style.display = 'none';
-	} else if ( users.length == 0 ) {
-		_gel("message").style.display = 'block';
-		_gel("list").style.display = 'none';
-		_gel("message").innerHTML = 'No user found'; // to be translated
-	} else {
-		_gel("message").style.display = 'none';
-		_gel("list").style.display = 'block';
+  if ( typeof users == 'undefined' ) {
+    _gel("message").style.display = 'block';
+    _gel("list").style.display = 'none';
+    _gel("message").innerHTML = prefs.getMsg('label.gadget.members.description');
+  } else if ( users.length == 0 ) {
+    _gel("message").style.display = 'block';
+    _gel("list").style.display = 'none';
+    _gel("message").innerHTML = prefs.getMsg('label.gadget.members.no.result');
+  } else {
+    _gel("message").style.display = 'none';
+    _gel("list").style.display = 'block';
 
-		_gel("pageInfo").innerHTML = (page + 1) + '/' + pageMax;
-		_gel("listData").innerHTML = buildList();
+    _gel("pageInfo").innerHTML = (page + 1) + '/' + pageMax;
+    _gel("listData").innerHTML = buildList();
 
     $(".user").click(function(){
       window.parent.location = $(this).find("a").attr("href");
       return false;
     });
-	}
-	gadgets.window.adjustHeight();
+  }
+  gadgets.window.adjustHeight();
 }
 
 function buildList() {
@@ -77,28 +78,28 @@ function buildList() {
 }
 
 function getUsers() {
-	NXRequestParams.operationParams.page = page;
-	doAutomationRequest(NXRequestParams);
+  NXRequestParams.operationParams.page = page;
+  doAutomationRequest(NXRequestParams);
 }
 
 
 // called when the gadget is loaded
 gadgets.util.registerOnLoadHandler(
-	function() {
-		_gel('navFirstPage').onclick = function(e) {
-			firstPage()
-    	};
-    	_gel('navPrevPage').onclick = function(e) {
-        	prevPage()
-    	};
-    	_gel('navNextPage').onclick = function(e) {
-        	nextPage()
-    	};
-    	_gel('navLastPage').onclick = function(e) {
-        	lastPage()
-    	};
-		gadgets.window.adjustHeight();
-	}
+  function() {
+    _gel('navFirstPage').onclick = function(e) {
+      firstPage()
+      };
+      _gel('navPrevPage').onclick = function(e) {
+          prevPage()
+      };
+      _gel('navNextPage').onclick = function(e) {
+          nextPage()
+      };
+      _gel('navLastPage').onclick = function(e) {
+          lastPage()
+      };
+    gadgets.window.adjustHeight();
+  }
 );
 
 function isEmpty(s) {
@@ -107,29 +108,29 @@ function isEmpty(s) {
 
 
 function firstPage(){
-	if ( users.length > 0 ) {
-		page = 0 ;
-		getUsers();
-	}
+  if ( users.length > 0 ) {
+    page = 0 ;
+    getUsers();
+  }
 }
 
 function prevPage(){
-	if ( users.length > 0 && page > 0) {
-		page -- ;
-		getUsers();
-	}
+  if ( users.length > 0 && page > 0) {
+    page -- ;
+    getUsers();
+  }
 }
 
 function nextPage(){
-	if ( users.length > 0 && page < (pageMax - 1)) {
-		page ++ ;
-		getUsers();
-	}
+  if ( users.length > 0 && page < (pageMax - 1)) {
+    page ++ ;
+    getUsers();
+  }
 }
 
 function lastPage(){
-	if ( page < pageMax - 1 ) {
-		page = pageMax -1 ;
-		getUsers();
-	}
+  if ( page < pageMax - 1 ) {
+    page = pageMax -1 ;
+    getUsers();
+  }
 }
