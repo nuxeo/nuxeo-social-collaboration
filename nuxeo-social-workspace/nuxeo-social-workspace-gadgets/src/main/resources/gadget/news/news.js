5d67a37db889ba587473f093ad0c5b340e428c88
Arnaud Kervern <akervern@nuxeo.com>
2011-12-13 17:49:56 +0100

    NXP-8096: Do not call toLocaleDateString on a string

--- nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/news/news.js
+++ nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/gadget/news/news.js
@@ -24,7 +24,7 @@ function displayListOfArticles(entries,nxParams){
       var entry = entries[i];
       var date = getDateForDisplay(entry.properties["dc:modified"]);
       html += '<div class="news">';
-      html += '<p class="infos">' + entry.properties["dc:creator"] + ', ' + date.toLocaleDateString() + ' - ';
+      html += '<p class="infos">' + entry.properties["dc:creator"] + ', ' + date + ' - ';
       html += '<a target ="_top" class="news_title" title="';
       html += entry.title;
       html += "\" href=\"";
