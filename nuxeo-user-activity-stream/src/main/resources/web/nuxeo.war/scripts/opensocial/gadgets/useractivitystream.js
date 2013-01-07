dea40dd35d5b3ce710e01c46c955d93946493ad9
Thomas Roger <troger@nuxeo.com>
2013-01-07 17:17:06 +0100

    NXP-10301: do not recompute activity icon URL when clicking on Show More in the user activity stream

--- nuxeo-user-activity-stream/src/main/resources/web/nuxeo.war/scripts/opensocial/gadgets/useractivitystream.js
+++ nuxeo-user-activity-stream/src/main/resources/web/nuxeo.war/scripts/opensocial/gadgets/useractivitystream.js
@@ -76,18 +76,6 @@
   function buildActivityHtml(template, activity) {
     var icon = activity.icon;
     if (icon != null && icon.length > 0) {
-      if (icon[0] == '/') {
-        icon = icon.substring(1);
-      }
-    } else {
-      icon = constants.noActivityTypeIcon;
-    }
-    if (icon.indexOf(NXGadgetContext.clientSideBaseUrl) < 0) {
-      icon = NXGadgetContext.clientSideBaseUrl + icon;
-    }
-    activity.icon = icon;
-    var icon = activity.icon;
-    if (icon != null && icon.length > 0) {
       if (icon.indexOf(NXGadgetContext.clientSideBaseUrl) < 0) {
         if (icon[0] == '/') {
           icon = icon.substring(1);
