fa38e3dea8db51421d27b8df02b706f3920722d3
Nelson Silva <nelson.silva@inevo.pt>
2013-01-07 12:34:04 +0000

    NXP-10301 do not recompute activity icon URL when clicking on Show More in activity stream

--- nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/web/nuxeo.war/scripts/opensocial/gadgets/socialworkspaceactivitystream.js
+++ nuxeo-social-workspace/nuxeo-social-workspace-gadgets/src/main/resources/web/nuxeo.war/scripts/opensocial/gadgets/socialworkspaceactivitystream.js
@@ -78,14 +78,14 @@ var prefs = new gadgets.Prefs();
   function buildActivityHtml(template, activity) {
     var icon = activity.icon;
     if (icon != null && icon.length > 0) {
-      if (icon[0] == '/') {
-        icon = icon.substring(1);
+      if (icon.indexOf(NXGadgetContext.clientSideBaseUrl) < 0) {
+        if (icon[0] == '/') {
+          icon = icon.substring(1);
+        }
+        icon = NXGadgetContext.clientSideBaseUrl + icon;
       }
     } else {
-      icon = constants.noActivityTypeIcon;
-    }
-    if (icon.indexOf(NXGadgetContext.clientSideBaseUrl) < 0) {
-      icon = NXGadgetContext.clientSideBaseUrl + icon;
+    	icon = NXGadgetContext.clientSideBaseUrl + constants.noActivityTypeIcon;
     }
     activity.icon = icon;
     return Mustache.render(template, activity);
