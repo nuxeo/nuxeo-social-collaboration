6b3e96684558cc8e4d363e08fcb07722eab3149e
Thierry Martins <tmartins@nuxeo.com>
2013-04-09 19:25:02 +0200

    NXP-11383: skip VisibilitySocialDocumentListener when source document is deleted

--- nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/VisibilitySocialDocumentListener.java
+++ nuxeo-social-workspace/nuxeo-social-workspace-core/src/main/java/org/nuxeo/ecm/social/workspace/listeners/VisibilitySocialDocumentListener.java
@@ -24,6 +24,7 @@ import static org.nuxeo.ecm.social.workspace.helper.SocialWorkspaceHelper.toSoci
 import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.ClientRuntimeException;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.event.DeletedDocumentModel;
 import org.nuxeo.ecm.core.event.Event;
 import org.nuxeo.ecm.core.event.EventBundle;
 import org.nuxeo.ecm.core.event.EventContext;
@@ -67,6 +68,10 @@ public class VisibilitySocialDocumentListener implements
             return;
         }
         DocumentModel document = ((DocumentEventContext) ctx).getSourceDocument();
+        if (document instanceof DeletedDocumentModel) {
+            return;
+        }
+
         if (ctx.hasProperty(ALREADY_PROCESSED)) {
             return;
         }
